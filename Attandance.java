ed1232d0ee8001b0e19ba5b770204fadd14abb06
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-15 20:11:06 +0530

    Added: Employee is prasent or absent

---
+++ Attandance.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.in;
+public class Attandance {
+
+        public static void main(String[] args)
+        {
+
+            int isFullTime=1;
+            double Empcheck= Math.floor(Math.random() *10)%2;
+
+            if(Empcheck==isFullTime)
+            {
+                System.out.println("Employee is Present");
+
+            }
+            else {
+                System.out.println("Employee is Absent");
+            }
+
+        }
+
+    }
