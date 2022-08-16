9def29da7bc3a9c3558a546970f0d086c7cb48ef
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-16 19:30:57 +0530

    Added: Employee wage

---
+++ EmpDailyWage.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.Day3Emp;
+
+public class EmpDailyWage {
+
+    public static void main(String[] args) {
+        int isFullTime=1;
+        int EmpHrs=0;
+        int EmpWage=0;
+        int EmpRatePerHr=20;
+
+        double EmpCheck = Math.floor(Math.random()*10)%2;
+        if (EmpCheck==isFullTime);
+        {
+            EmpHrs=8;
+        }
+        EmpWage = EmpHrs * EmpRatePerHr;
+        System.out.println(" EmpWage "+EmpWage);
+
+    }}
---
+++ WageConditionForMonth.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.Day3Emp;
+
+public class WageConditionForMonth {
+    public static void main(String[] args)
+    {
+
+        int isFullTime=1;
+        int isPartTime=2;
+        int EMHrs=0;
+        int EMWage=0;
+        int EMRatPrHrs=20;
+        int TotalDayWage=20;
+        int TotalHrs=100;
+
+        double EmpCheck = Math.floor(Math.random()*10)%2;
+
+        if (EmpCheck == isFullTime)
+        {
+            EMHrs=8;
+
+        }
+        else if (EmpCheck == isPartTime)
+        {
+            EMHrs=4;
+        }
+
+        EMWage = EMHrs*EMRatPrHrs*TotalDayWage*TotalHrs;
+        System.out.println("Total Working Hours in Month "+EMWage);
+
+    }
+}
